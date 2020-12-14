bb8548d12383c5bcae136b1d6dfbd0f27631a6d9
Cristiano <cristiano.tarconi@gmail.com>
2020-12-15 00:36:53 +0100

    final adjs

--- js/script.js
+++ js/script.js
@@ -4,37 +4,42 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65 */
 
-var name, surname, age, km, rate;
+// VARIABILI
+var name, surname, age, km, rate, finalPrice, discount1, discount2, coupon1, coupon2;
 
+// Input da inserire
 name = prompt('Inserisci il tuo nome');
 surname = prompt('Inserisci il tuo cognome');
 age = parseInt(prompt('Inserisci la tua età'));
 km = parseInt(prompt('Inserisci il numero di chilometri da percorrere'));
+
+// Variabile standard per capire il costo per km
 rate = 0.21;
 
+// Calcolo prezzo biglietto di listini - senza sconto
+basePrice = km * rate;
+
+// Sconto + COUPON UNDER 18
+discount1 = basePrice - (basePrice * 20 / 100);
+coupon1 = 'DHE1PB';
+// Sconto + COUPON OVER 65
+discount2 = basePrice - (basePrice * 40 / 100);
+coupon2 = 'EPT6YF';
+
+// Condizione per capire quale percentuale di sconto applicare
+if (age < 18) {
+    finalPrice = discount1;
+    document.getElementById('coupon').innerHTML = coupon1;
+} else if (age >= 65) {
+    finalPrice = discount2
+    document.getElementById('coupon').innerHTML = coupon2;
+} else {
+    finalPrice = basePrice;
+}
+
+// Visualizzazione su documento HTML
+document.getElementById('price').innerHTML = finalPrice.toFixed(2);
 document.getElementById('name').innerHTML = name;
 document.getElementById('surname').innerHTML = surname;
 document.getElementById('age').innerHTML = age;
 document.getElementById('km').innerHTML = km;
-
-basePrice = km * rate;
-
-console.log(basePrice.toFixed(2) + '€');
-
-
-var discount1 = basePrice - (basePrice * 20 / 100);
-var discount2 = basePrice - (basePrice * 40 / 100);
-
-var coupon1 = 'DHE1PB';
-var coupon2 = 'EPT6YF';
-
-
-if (age < 18) {
-    document.getElementById('price').innerHTML = discount1.toFixed(2);
-    document.getElementById('coupon').innerHTML = coupon1;
-} else if (age > 65) {
-    document.getElementById('price').innerHTML = discount2.toFixed(2);
-    document.getElementById('coupon').innerHTML = coupon2;
-} else {
-    document.getElementById('price').innerHTML = basePrice.toFixed(2);
-}
