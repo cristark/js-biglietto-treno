e6ca64e6da0a092b51b6c8d6415795698fbf8127
Cristiano <cristiano.tarconi@gmail.com>
2020-12-14 17:44:33 +0100

    style structure + js connected

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@ va applicato uno sconto del 40% per gli over 65 */
 
 var name, surname, age, km, rate;
 
-// name = prompt('Inserisci il tuo nome');
-// surname = prompt('Inserisci il tuo cognome');
+name = prompt('Inserisci il tuo nome');
+surname = prompt('Inserisci il tuo cognome');
 age = parseInt(prompt('Inserisci la tua età'));
 km = parseInt(prompt('Inserisci il numero di chilometri da percorrere'));
 rate = 0.21;
 
-console.log(name);
-console.log(surname);
-console.log(age);
-console.log(km);
+document.getElementById('name').innerHTML = name;
+document.getElementById('surname').innerHTML = surname;
+document.getElementById('age').innerHTML = age;
+document.getElementById('km').innerHTML = km;
 
 basePrice = km * rate;
 
@@ -25,11 +25,16 @@ console.log(basePrice.toFixed(2) + '€');
 var discount1 = basePrice - (basePrice * 20 / 100);
 var discount2 = basePrice - (basePrice * 40 / 100);
 
+var coupon1 = 'DHE1PB';
+var coupon2 = 'EPT6YF';
+
 
 if (age < 18) {
     document.getElementById('price').innerHTML = discount1;
+    document.getElementById('coupon').innerHTML = coupon1;
 } else if (age > 65) {
     document.getElementById('price').innerHTML = discount2;
+    document.getElementById('coupon').innerHTML = coupon2;
 } else {
     document.getElementById('price').innerHTML = basePrice;
 }
